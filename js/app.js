e1c65b7797aa270fff1eb8dc008a6af25c81bdfc
ammarib <ibmammar@gmail.com>
2021-01-05 01:54:08 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
 'use strict';
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-var shops  = [];
+var shops = [];
 
-function Salmon(name, min, max, avrg) {
-  this.name = name;
-  this.min = min;
-  this.max = max;
-  this.avrg = avrg;
-  this.hourlyArray = [];
-  this.total = 0;
-  shops.push(this);
-  this.cookies();
+function Salmon(
+    name,
+    min,
+    max,
+    avrg) {
+    this.name = name;
+    this.min = min;
+    this.max = max;
+    this.avrg = avrg;
+    this.hourlyArray = [];
+    this.total = 0;
+    shops.push(this);
+    this.cookies();
 }
 function randomSales(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 Salmon.prototype.cookies = function () {
-  for (let index = 0; index < hours.length; index++) {
-    var cookiesPerHour = Math.floor((randomSales(this.min, this.max) * this.avrg));
-    this.hourlyArray.push(cookiesPerHour);
-    this.total += this.hourlyArray[index];
-  }
+    for (let index = 0; index < hours.length; index++) {
+        var cookiesPerHour = Math.floor((randomSales(this.min, this.max) * this.avrg));
+        this.hourlyArray.push(cookiesPerHour);
+        this.total += this.hourlyArray[index];
+    }
 
 };
 Salmon.prototype.render = function () {
@@ -32,7 +36,7 @@ Salmon.prototype.render = function () {
     var tData = document.createElement('td');
     tr1.appendChild(tData);
     tData.textContent = this.name;
-    for(var x = 0; x < hours.length ; x++){
+    for (var x = 0; x < hours.length; x++) {
         tData = document.createElement('td');
         tr1.appendChild(tData);
         tData.textContent = this.hourlyArray[x];
@@ -48,32 +52,32 @@ container.appendChild(table1);
 
 var seattle = new Salmon('Seatle', '23', '65', '6.3');
 console.log(seattle);
-var tokyo =new Salmon('Tokyo', '3', '24', '1.2');
+var tokyo = new Salmon('Tokyo', '3', '24', '1.2');
 console.log(tokyo);
-var dubai =new Salmon('Dubai', '11', '38', '3.7');
+var dubai = new Salmon('Dubai', '11', '38', '3.7');
 console.log(dubai);
 var paris = new Salmon('Paris', '20', '38', '2.3');
 console.log(paris);
 var lima = new Salmon('Lima', '2', '16', '4.6');
 console.log(lima);
 
-function renderHeader(){
-  var tr1 = document.createElement('tr');
-  table1.appendChild(tr1);
-  var tableHead = document.createElement('th');
-  tableHead.textContent = ' ';
-  tr1.appendChild(tableHead);
-  for(var i = 0 ; i< hours.length ; i++){
-    var tableHou = document.createElement('th');
-    tableHou.textContent = hours[i];
-    tr1.appendChild(tableHou);
-  }
-  var theTotals = document.createElement('th');
-  theTotals.textContent = 'Total';
-  tr1.appendChild(theTotals);
+function headerRend() {
+    var tr1 = document.createElement('tr');
+    table1.appendChild(tr1);
+    var tableHead = document.createElement('th');
+    tableHead.textContent = ' ';
+    tr1.appendChild(tableHead);
+    for (var i = 0; i < hours.length; i++) {
+        var tableHou = document.createElement('th');
+        tableHou.textContent = hours[i];
+        tr1.appendChild(tableHou);
+    }
+    var theTotals = document.createElement('th');
+    theTotals.textContent = 'Total';
+    tr1.appendChild(theTotals);
 
 }
-function renderFooter(){
+function footerRend() {
     var tr1 = document.createElement('tr');
     table1.appendChild(tr1);
     var tData = document.createElement('td');
@@ -81,10 +85,10 @@ function renderFooter(){
     tData.textContent = 'Total';
     var totalHours;
     var finalTotal = 0;
-    for(var h = 0 ; h < hours.length ; h++){
+    for (var h = 0; h < hours.length; h++) {
         totalHours = 0;
-        for(var shop = 0 ; shop < shops.length ; shop++){
-            totalHours += shops[shop].hourlyArray[h];
+        for (var c = 0; c < shops.length; c++) {
+            totalHours += shops[c].hourlyArray[h];
         }
         var totalDataTaHou = document.createElement('td');
         totalDataTaHou.textContent = totalHours;
@@ -96,11 +100,11 @@ function renderFooter(){
     tr1.appendChild(tdAllTotals);
 
 }
-renderHeader();
-for(var i = 0 ; i < shops.length ; i++){
-  shops[i].render();
+headerRend();
+for (var i = 0; i < shops.length; i++) {
+    shops[i].render();
 }
-renderFooter();
+footerRend();
 // var Seattle = {
 //     location: 'Seattle',
 //     min: 23,
